2def7a3a2e1a167d687a895a13248a35f176dd35
mark.rudman <mark@boyofbeard.com>
2016-01-15 15:17:39 +0000

    Gruntfile update
    Watch task added to default command

--- gruntfile.js
+++ gruntfile.js
@@ -70,5 +70,5 @@ module.exports = function(grunt) {
 
     // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
 
-    grunt.registerTask('default', ['sass', 'postcss:dist', 'cssnano']);
+    grunt.registerTask('default', ['sass', 'postcss:dist', 'cssnano', 'watch']);
 };
